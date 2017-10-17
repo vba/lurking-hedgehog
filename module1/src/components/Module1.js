c42a161ff9305dc28808c3d8ac7d95198a309532
Victor Bartel <victor.bartel@renault.com>
2017-10-17 14:31:37 +0200

    Add google maps integration

--- module1/src/components/Module1.js
+++ module1/src/components/Module1.js
@@ -1,5 +1,6 @@
 
 import React, { Component } from 'react';
+import MapView from 'react-native-maps';
 import {chooseService} from '../actions';
 import { Image, View, Text, Button, StyleSheet } from 'react-native';
 import {connect} from 'react-redux'
@@ -7,25 +8,40 @@ import {connect} from 'react-redux'
 class Module1 extends Component {
     onPressLearnMore = () =>  this.props.chooseService(2, `Hello from Celtics, bro at ${new Date().toTimeString()}`);
     render() {
-        let pic = require('./resources/celtic.jpg');
+        // const { region } = this.props;
+        // console.log(region);
         return (
             <View>
-                <View>
-                    <Text>
-                        It's Celtic bro!
-                    </Text>
-                    <Image source={pic} style={{ width: 250, height: 250 }} />
-                    <Button onPress={this.onPressLearnMore.bind(this)}
-                            title="Press on me"
-                            color="#841584"/>
+                <View style={styles.container}>
+                    <MapView
+                        style={styles.map}
+                        initialRegion={{
+                            latitude: 48.856614,
+                            longitude: 2.3522219,
+                            latitudeDelta: 0.01922,
+                            longitudeDelta: 0.00921,
+                        }}
+                    />
                 </View>
+                <Button onPress={this.onPressLearnMore.bind(this)}
+                        title="Press on me"
+                        color="#841584"/>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        // ...StyleSheet.absoluteFillObject,
+        height: 320,
+        width: 350,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
 });
 
 const mapStateToProps = state => {
